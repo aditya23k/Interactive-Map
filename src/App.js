0bdd2c3209bace5a108f0237d88c8598753fd373
Aditya <46108348+aditya23k@users.noreply.github.com>
2023-01-08 22:40:46 +0530

    Added the code with the map and the tooltip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React ,{useState} from "react"
+import React ,{useState,useEffect} from "react"
 
 import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+import { ZoomableGroup } from "react-simple-maps"
+//import { Marker } from "react-simple-maps"
 
-import ReactTooltip from "react-tooltip";
+import {Tooltip as ReactTooltip} from "react-tooltip";
 
 const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 
 export default function App() {
+
+  const[countries,setCountries] = useState([])
+
+  const getData = () =>{
+    fetch('http://localhost:3004/countries', {
+       header:{
+        'Content-Type' : 'application/json',
+        'Accept' : 'application/json'
+       }
+    })
+    .then(res => res.json())
+    .then((data) => {setCountries(data)})
+
+  }
+  useEffect(() => {
+    getData()
+  },[])
+
   return (
     <div
       className = 'App'
@@ -20,8 +40,37 @@ export default function App() {
         alignItems:"centre",
       }}
     >
-      <h1>Let's Build an Awesome Map!</h1>
-
+      
+      <h1><ReactTooltip>{countries}</ReactTooltip></h1>
+      <div style={{width:'700px' , borderStyle:'double'}}>
+        <ComposableMap>
+          <ZoomableGroup zoom={1}>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                <Geography 
+                  key={geo.rsmKey} 
+                  geography={geo} 
+                  onMouseEnter ={() => {
+                    const {name} = geo.properties
+                    setCountries(`${name}`);
+ 
+                  }}
+                  onMouseLeave = {() =>{
+                    setCountries("");
+                  }} 
+                  style ={{
+                    hover: {
+                      fill: "#F53",
+                      outline:'none' 
+                    }
+                  }}/>
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup> 
+        </ComposableMap>
+      </div>
     </div>
     
   )
